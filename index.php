442ef4d052efeb16966085ebce684ec37bac70dd
maniot <maniot@maniot.com>
2017-03-08 11:35:44 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,301 @@
+
+<?php
+
+//Pass variables to determine the type of view
+
+// Functions
+// period - specify what timeperiods you want to see
+
+//Set Variables to null to start
+$period="";
+$chart_rounding="";
+
+//Check input for badness
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+#Check to see if the neccessary variables exist
+
+if(isset($_GET["period"])) {
+    // type exists
+    if (empty($_GET["period"])) {
+        // Default to Day if no period is set or empty
+        $period = "today";
+        } else {
+            $period = test_input($_GET["period"]);
+        }
+    } else {
+    $period = "today";
+}
+
+#Refresh our index.php page every 5 minutes so we get the latest data and can follow along during the day.
+$page = $_SERVER['PHP_SELF'];
+$sec = "300";
+header("Refresh: $sec; url=$page");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+    <!-- Header and Navigation -->
+         <?php include($_SERVER["DOCUMENT_ROOT"] . "/include/navigation.php"); ?>
+    <!-- /Navigation -->
+
+
+
+    <div id="wrapper">
+<?php include "datawidgets/currentconditions.php"; ?>
+        
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Dashboard - <?php echo "$recorddate"; ?></h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <!-- Top Row -->
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <img src="../images/temp.png" width="75" height="75">
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="h4">
+                                    <?php 
+                                    
+                                    if ($hivetempf == "null" ) {
+                                        $hivetempf="NA"; 
+                                    }
+                                    echo "$hivetempf ";
+
+                                    if ($SHOW_METRIC == "on") {
+                                        echo " C"; $i = "C"; 
+                                        } 
+                                    else { 
+                                         echo "F"; $i = "F";
+                                     } 
+                                if ($wxtempf == "null" ) {
+                                        $wxtempf="NA"; 
+                                    }
+                                        echo " / "."$wxtempf"." $i"; 
+                                        ?> </div>
+                                    <div>Temp - Hive / Ambient</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="/pages/temp.php?chart=line&period=week">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-hiveyellow">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                     <img src="/images/scalesm.png">
+                                </div>
+                                <div class="col-xs-9 text-right">
+            
+                                    <div class="h4"><?php echo "$hiveweight "; if ($SHOW_METRIC == "on") { echo " kg"; $i = "kg"; } else {echo "lb"; $i = "lb";} echo " / "."$changeweight"." $i"; ?> </div>
+                                    <div>Weight / Trend </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="/pages/weight.php?chart=line&period=week">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                     <img src="../images/hum.png" width="75" height="75">
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="h4"><?php 
+                                    if ($hivehumi == "null" ) {
+                                        $hivehumi="NA"; 
+                                    }
+                                    if ($wxhumi == "null" ) {
+                                        $wxhumi="NA"; 
+                                    }
+
+                                    echo "$hivehumi"." / "."$wxhumi"; ?>%</div>
+                                    <div>Humidity - Hive / Ambient</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="/pages/temp.php?chart=line&period=week">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-hivebrown">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <img src="../images/treesm.png" width="75" height="75">
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="h4"><?PHP echo "$daygdd / $seasongdd"; ?></div>
+                                    <div>GDD - Day/Season</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="/pages/gdd.php?chart=line&period=week">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- /.Top Row -->
+            <!-- New Row 1 -->
+            <div class="row">
+                <div class="col-lg-12">
+        
+        <!-- Button Bar -->
+            <a href="/pages/index.php?period=today"><button type="button" class="btn btn btn-<?PHP if ($period == "today"){echo "primary";} else {echo "default";}?>">Today</button></a>
+            <a href="/pages/index.php?period=day"><button type="button" class="btn btn btn-<?PHP if ($period == "day"){echo "primary";} else {echo "default";}?>">24 hrs</button></a>
+            <a href="/pages/index.php?period=week"><button type="button" class="btn btn btn-<?PHP if ($period == "week"){echo "primary";} else {echo "default";}?>">Week</button></a>
+            <a href="/pages/index.php?period=month"><button type="button" class="btn btn btn-<?PHP if ($period == "month"){echo "primary";} else {echo "default";}?>">Month</button></a>
+            <a href="/pages/index.php?period=year"><button type="button" class="btn btn btn-<?PHP if ($period == "year"){echo "primary";} else {echo "default";}?>">Year</button></a>
+            <a href="/pages/index.php?period=all"><button type="button" class="btn btn btn-<?PHP if ($period == "all"){echo "primary";} else {echo "default";}?>">All</button></a>
+            <br>
+                </div>
+                <!-- /.col-lg-12 -->
+                        </div>
+            <div class="row">
+            
+            <?php
+            if ($SITE_TYPE == "normal") {
+                echo '<div class="col-lg-8">';
+            }
+            if ($SITE_TYPE == "compact") {
+                echo '<div class="col-lg-12">';
+            }
+            ?>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Hive Charts 
+                        </div>
+                        <div class="panel-body">
+                            <div class="pull-center" id="allcontainer"></div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.col-lg-6 -->
+            <?php
+            if ($SITE_TYPE == "normal") {
+                echo '
+            
+                <div class="col-lg-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Estimated Stores (Honey,Pollen,Nectar) 
+                        </div>
+                        <div class="panel-body">
+                            <div class="pull-center" id="weightgauge-container"></div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            System Status 
+                        </div>
+                        <div class="panel-body">';
+            include "status.php"; 
+                        echo '</div>
+
+                    </div>
+
+                </div>
+                ';} ?>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+            <!-- /.row -->                
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+
+                <!-- /.col-lg-6 -->
+            </div>
+            <!-- /.row -->
+                            </div>
+                        </div>
+                        <!-- /.panel-footer -->
+                    </div>
+                    <!-- /.panel .chat-panel -->
+                </div>
+                <!-- /.col-lg-4 -->
+            </div>
+            <!-- /.Row1 -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    
+    <script src="/js/highcharts/highcharts.js"></script>
+    <script src="/js/highcharts/modules/exporting.js"></script>
+
+  <!-- Full Screen Popups -->
+    <script src="/js/popup.js"></script>    
+
+  <?php 
+
+  
+  include "datawidgets/all_chart.php"; 
+
+  include "datawidgets/weightguage-hc.php"; 
+
+  ?> 
+  
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
---
+++ navigation.php
@@ -0,0 +1,197 @@
+ <!-- Pull Config -->
+
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/include/db-connect.php");
+
+#Get Site Orient and Options
+$sth3 = $conn->prepare("SELECT SITE_ORIENT,HIVENAME,SITE_TYPE,SHOW_METRIC FROM hiveconfig");
+$sth3->execute();
+$result3 = $sth3->fetch(PDO::FETCH_ASSOC);
+
+$orient = $result3['SITE_ORIENT'];
+$id = $result3['HIVENAME'];
+$SITE_TYPE = $result3['SITE_TYPE'];
+$SHOW_METRIC = $result3['SHOW_METRIC'];
+
+# =====================================================================
+# Set the header
+# =====================================================================
+ echo '
+    <head>
+   <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Application to manage hive management tasks, and to report on various instruments.">
+    <meta name="author" content="root" >';
+ echo '<title> - Hive Control</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="../dist/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+    
+    <!-- Custom Fonts -->
+    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesnt work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="wrapper">
+';
+
+
+#=====================
+# Show either wide/or normal navigations
+# Note, two different sets below
+#=======================
+
+echo '<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <h4 class="text-muted"><img src="/images/hive2.png"> Hive Control - ';  echo "$id"; echo '</h4>
+            </div>
+            <!-- /.navbar-header -->
+
+            <!-- /.navbar-top-links -->';
+
+#<!-- Wide Orient -->
+
+if ($orient == "wide") {
+echo '<ul class="nav navbar-top-links navbar-right">
+         <li><a href="/pages/index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+         </li>
+         <li class="dropdown">
+             <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-bar-chart-o fa-fw"></i> Detail <i class="fa fa-caret-down"> </i>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/pages/temp.php?chart=line&period=week">Temp/Humidity</a>
+                        </li>
+                        <li><a href="/pages/weight.php?chart=line&period=week">Weight</a>
+                        </li>
+                        <li><a href="/pages/light.php?chart=line&period=week">Light</a>
+                        </li>
+                        <li><a href="/pages/gdd.php?chart=line&period=week">GDD</a>
+                        </li>
+                        <li><a href="/pages/beecount.php?chart=line&period=week">Flights</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+            
+                 <li><a href="/pages/environment.php"><i class="fa fa-table fa-fw"></i>Weather</a>
+                 </li>
+                 <li><a href="/pages/video.php"><i class="fa fa-video-camera fa-fw"></i>Camera</a>
+                 </li>
+                 <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-gear fa-fw"></i> Settings <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/admin/hiveconfig.php">Basic Config</a>
+                        </li>
+                        <li><a href="/admin/instrumentconfig.php">Instruments</a>
+                        </li>
+                        <li><a href="/admin/hivebodyconfig.php">Hive Components</a>
+                        </li>
+                        <li><a href="/admin/siteconfig.php">Site Preferences</a>
+                        </li>
+                        <li><a href="/admin/system.php">System Commands</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+            </ul>
+            <!-- /.navbar-top-links -->
+
+<div class="well">';
+}
+
+
+if ($orient == "normal") {
+
+#<!-- Normal Orient -->
+ 
+ echo '
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li>
+                            <a href="/pages/index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i>Data Analysis<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="/pages/temp.php?chart=line&period=week">Temp/Humidity</a>
+                                </li>
+                                <li>
+                                    <a href="/pages/weight.php?chart=line&period=week">Weight</a>
+                                </li>
+                                <li>
+                                    <a href="/pages/light.php?chart=line&period=week">Light</a>
+                                </li>
+                                <li>
+                                    <a href="/pages/gdd.php?chart=line&period=week">GDD</a>
+                                </li>
+                                <li>
+                                     <a href="/pages/beecount.php?chart=line&period=week">flights</a>
+                                </li>
+                                
+                            </ul>
+                        <li>
+                            <a href="/pages/environment.php"><i class="fa fa-table fa-fw"></i> Weather</a>
+                        </li>
+                        <li>
+                            <a href="/pages/video.php"><i class="fa fa-video-camera fa-fw"></i> Live WebCam</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-gear fa-fw"></i>Settings<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="/admin/hiveconfig.php">Basic Config</a>
+                                </li>
+                                <li>
+                                    <a href="/admin/instrumentconfig.php">Instruments</a>
+                                </li>
+                                <li>
+                                    <a href="/admin/hivebodyconfig.php">Hive Body</a>
+                                </li>
+                                <li>
+                                    <a href="/admin/siteconfig.php">Site Preferences</a>
+                                </li>
+                                <li>
+                                    <a href="/admin/system.php">System Commands</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        
+   
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+        <div id="page-wrapper">
+        ';
+}?>
+        
